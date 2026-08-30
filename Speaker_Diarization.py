15a9862f54784c52228b2650c44a5e286557410f
Tuan Anh <dtuan0305@gmail.com>
2026-08-30 15:58:13 +0700

    update code

--- Speaker_Diarization.py
+++ Speaker_Diarization.py
@@ -4,13 +4,13 @@ import numpy as np
 
 def speaker_diarization(audio_path):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1")
+    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-community-1")
     pipeline=pipeline.to(device)
     output = pipeline(audio_path,min_speakers=2,max_speakers=10)
 
     diarization = output.speaker_diarization
     segments = []
-    for turn, _, speaker in diarization.itertracks(yield_label=True):
+    for turn, speaker in diarization.itertracks(yield_label=True):
         segments.append({
             "speaker": speaker,
             "start": round(turn.start, 2),
