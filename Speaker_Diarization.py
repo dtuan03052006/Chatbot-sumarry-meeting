4bf83077dc4e33595492dd019727e8a62bd443f5
Tuan Anh <dtuan0305@gmail.com>
2026-08-31 14:24:43 +0700

    Update code

--- Speaker_Diarization.py
+++ Speaker_Diarization.py
@@ -8,9 +8,9 @@ def speaker_diarization(audio_path):
     pipeline=pipeline.to(device)
     output = pipeline(audio_path,min_speakers=2,max_speakers=10)
 
-    diarization = output.speaker_diarization
+
     segments = []
-    for turn,_, speaker in diarization.itertracks(yield_label=True):
+    for turn,_, speaker in output.itertracks(yield_label=True):
         segments.append({
             "speaker": speaker,
             "start": round(turn.start, 2),
